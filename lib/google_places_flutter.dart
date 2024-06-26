e7937319c94d88e70603d5ef70730f03c723c476
Mariam Gustavo <yegetamariam@gmail.com>
2024-06-26 23:13:04 +0300

    customized google_places_flutter package inorder to use a server endpoint that calls google maps api

---
+++ lib/google_places_flutter.dart
@@ -0,0 +1,331 @@
+library google_places_flutter;
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_places_flutter/model/place_details.dart';
+import 'package:google_places_flutter/model/prediction.dart';
+
+import 'package:rxdart/subjects.dart';
+import 'package:dio/dio.dart';
+import 'package:rxdart/rxdart.dart';
+
+import 'DioErrorHandler.dart';
+
+class GooglePlaceAutoCompleteTextField extends StatefulWidget {
+  InputDecoration inputDecoration;
+  ItemClick? itemClick;
+  GetPlaceDetailswWithLatLng? getPlaceDetailWithLatLng;
+  bool isLatLngRequired = true;
+
+  TextStyle textStyle;
+  String? googleAPIKey;
+  int debounceTime = 600;
+  List<String>? countries = [];
+  TextEditingController textEditingController = TextEditingController();
+  ListItemBuilder? itemBuilder;
+  Widget? seperatedBuilder;
+  void clearData;
+  BoxDecoration? boxDecoration;
+  bool isCrossBtnShown;
+  bool showError;
+  double? containerHorizontalPadding;
+  double? containerVerticalPadding;
+  FocusNode? focusNode;
+
+  GooglePlaceAutoCompleteTextField(
+      {required this.textEditingController,
+      this.googleAPIKey: "",
+      this.debounceTime: 600,
+      this.inputDecoration: const InputDecoration(),
+      this.itemClick,
+      this.isLatLngRequired = true,
+      this.textStyle: const TextStyle(),
+      this.countries,
+      this.getPlaceDetailWithLatLng,
+      this.itemBuilder,
+      this.boxDecoration,
+      this.isCrossBtnShown = true,
+      this.seperatedBuilder,
+      this.showError = true,
+      this.containerHorizontalPadding,
+      this.containerVerticalPadding,
+      this.focusNode});
+
+  @override
+  _GooglePlaceAutoCompleteTextFieldState createState() =>
+      _GooglePlaceAutoCompleteTextFieldState();
+}
+
+class _GooglePlaceAutoCompleteTextFieldState
+    extends State<GooglePlaceAutoCompleteTextField> {
+  final subject = new PublishSubject<String>();
+  OverlayEntry? _overlayEntry;
+  List<Prediction> alPredictions = [];
+
+  TextEditingController controller = TextEditingController();
+  final LayerLink _layerLink = LayerLink();
+  bool isSearched = false;
+
+  bool isCrossBtn = true;
+  late var _dio;
+
+  CancelToken? _cancelToken = CancelToken();
+
+  @override
+  Widget build(BuildContext context) {
+    return CompositedTransformTarget(
+      link: _layerLink,
+      child: Container(
+        padding: EdgeInsets.symmetric(
+            horizontal: widget.containerHorizontalPadding ?? 0,
+            vertical: widget.containerVerticalPadding ?? 0),
+        alignment: Alignment.centerLeft,
+        decoration: widget.boxDecoration ??
+            BoxDecoration(
+                shape: BoxShape.rectangle,
+                border: Border.all(color: Colors.grey, width: 0.6),
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: TextFormField(
+                decoration: widget.inputDecoration,
+                style: widget.textStyle,
+                controller: widget.textEditingController,
+                focusNode: widget.focusNode ?? FocusNode(),
+                onChanged: (string) {
+                  subject.add(string);
+                  if (widget.isCrossBtnShown) {
+                    isCrossBtn = string.isNotEmpty ? true : false;
+                    setState(() {});
+                  }
+                },
+              ),
+            ),
+            (!widget.isCrossBtnShown)
+                ? SizedBox()
+                : isCrossBtn && _showCrossIconWidget()
+                    ? IconButton(onPressed: clearData, icon: Icon(Icons.close))
+                    : SizedBox()
+          ],
+        ),
+      ),
+    );
+  }
+
+  getLocation(String test) async {
+    String apiURL =
+        "https://admin.e2store.net/api/map/autocomplete?search=$test";
+
+    if (widget.countries != null) {
+      for (int i = 0; i < widget.countries!.length; i++) {
+        String country = widget.countries![i];
+
+        if (i == 0) {
+          apiURL = apiURL + "&components=country:$country";
+        } else {
+          apiURL = apiURL + "|" + "country:" + country;
+        }
+      }
+    }
+
+    if (_cancelToken?.isCancelled == false) {
+      _cancelToken?.cancel();
+      _cancelToken = CancelToken();
+    }
+
+    try {
+      String url = apiURL;
+      Response response = await _dio.get(url);
+      ScaffoldMessenger.of(context).hideCurrentSnackBar();
+
+      Map map = response.data;
+      if (map.containsKey("error_message")) {
+        throw response.data;
+      }
+
+      PlacesAutocompleteResponse subscriptionResponse =
+          PlacesAutocompleteResponse.fromJson(response.data["data"]);
+
+      if (test.length == 0) {
+        alPredictions.clear();
+        this._overlayEntry!.remove();
+        return;
+      }
+
+      isSearched = false;
+      alPredictions.clear();
+      if (subscriptionResponse.predictions!.length > 0 &&
+          (widget.textEditingController.text.toString().trim()).isNotEmpty) {
+        alPredictions.addAll(subscriptionResponse.predictions!);
+      }
+
+      this._overlayEntry = null;
+      this._overlayEntry = this._createOverlayEntry();
+      Overlay.of(context)!.insert(this._overlayEntry!);
+    } catch (e) {
+      var errorHandler = ErrorHandler.internal().handleError(e);
+      _showSnackBar("${errorHandler.message}");
+      if (e is DioException) {}
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _dio = Dio();
+    subject.stream
+        .distinct()
+        .debounceTime(Duration(milliseconds: widget.debounceTime))
+        .listen(textChanged);
+  }
+
+  textChanged(String text) async {
+    getLocation(text);
+  }
+
+  OverlayEntry? _createOverlayEntry() {
+    if (context != null && context.findRenderObject() != null) {
+      RenderBox renderBox = context.findRenderObject() as RenderBox;
+      var size = renderBox.size;
+      var offset = renderBox.localToGlobal(Offset.zero);
+      return OverlayEntry(
+          builder: (context) => Positioned(
+                left: offset.dx,
+                top: size.height + offset.dy,
+                width: size.width,
+                child: CompositedTransformFollower(
+                  showWhenUnlinked: false,
+                  link: this._layerLink,
+                  offset: Offset(0.0, size.height + 5.0),
+                  child: Material(
+                      child: ListView.separated(
+                    padding: EdgeInsets.zero,
+                    shrinkWrap: true,
+                    itemCount: alPredictions.length,
+                    separatorBuilder: (context, pos) =>
+                        widget.seperatedBuilder ?? SizedBox(),
+                    itemBuilder: (BuildContext context, int index) {
+                      return InkWell(
+                        onTap: () {
+                          print("tapped");
+                          var selectedData = alPredictions[index];
+                          if (index < alPredictions.length) {
+                            // widget.itemClick!(selectedData);
+                            if (widget.isLatLngRequired) {
+                              getPlaceDetailsFromPlaceId(selectedData);
+                            }
+                            widget.itemClick!(selectedData);
+
+                            removeOverlay();
+                          }
+                        },
+                        child: widget.itemBuilder != null
+                            ? widget.itemBuilder!(
+                                context, index, alPredictions[index])
+                            : Container(
+                                padding: EdgeInsets.all(10),
+                                child: Text(alPredictions[index].description!)),
+                      );
+                    },
+                  )),
+                ),
+              ));
+    }
+  }
+
+  removeOverlay() {
+    alPredictions.clear();
+    this._overlayEntry = this._createOverlayEntry();
+    if (context != null) {
+      Overlay.of(context).insert(this._overlayEntry!);
+      this._overlayEntry!.markNeedsBuild();
+    }
+  }
+
+  Future<Response?> getPlaceDetailsFromPlaceId(Prediction prediction) async {
+    //String key = GlobalConfiguration().getString('google_maps_key');
+
+    var url =
+        "https://admin.e2store.net/api/map/placeDetails?placeid=${prediction.placeId}";
+    print("place iddd ${prediction.placeId}");
+    try {
+      Response response = await _dio.get(
+        url,
+      );
+
+      print(
+          "get details checking response.data data .predictions ${response.data["data"]}");
+
+      PlaceDetails placeDetails =
+          //TODO: check on package has this been changed
+          // PlaceDetails.fromJson(response.data.predictions);
+          // this is the original
+          PlaceDetails.fromJson(response.data["data"]);
+      prediction.lat = placeDetails.result!.geometry!.location!.lat.toString();
+      prediction.lng = placeDetails.result!.geometry!.location!.lng.toString();
+      print("latitude DETAILS${prediction.lat}");
+      print("longitude DETAILS${prediction.lng}");
+      print(
+          "printing widget.getPlaceDetailsWIthLatLng${widget.getPlaceDetailWithLatLng}");
+      widget.getPlaceDetailWithLatLng!(prediction);
+    } catch (e) {
+      var errorHandler = ErrorHandler.internal().handleError(e);
+      print("checking error location details  id 1 ${e}");
+      _showSnackBar("${errorHandler.message}");
+    }
+  }
+
+  void clearData() {
+    widget.textEditingController.clear();
+    if (_cancelToken?.isCancelled == false) {
+      _cancelToken?.cancel();
+    }
+
+    setState(() {
+      alPredictions.clear();
+      isCrossBtn = false;
+    });
+
+    if (this._overlayEntry != null) {
+      try {
+        this._overlayEntry?.remove();
+      } catch (e) {}
+    }
+  }
+
+  _showCrossIconWidget() {
+    return (widget.textEditingController.text.isNotEmpty);
+  }
+
+  _showSnackBar(String errorData) {
+    if (widget.showError) {
+      final snackBar = SnackBar(
+        content: Text("$errorData"),
+      );
+
+      // Find the ScaffoldMessenger in the widget tree
+      // and use it to show a SnackBar.
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+  }
+}
+
+PlacesAutocompleteResponse parseResponse(Map responseBody) {
+  return PlacesAutocompleteResponse.fromJson(
+      responseBody as Map<String, dynamic>);
+}
+
+PlaceDetails parsePlaceDetailMap(Map responseBody) {
+  return PlaceDetails.fromJson(responseBody as Map<String, dynamic>);
+}
+
+typedef ItemClick = void Function(Prediction postalCodeResponse);
+typedef GetPlaceDetailswWithLatLng = void Function(
+    Prediction postalCodeResponse);
+
+typedef ListItemBuilder = Widget Function(
+    BuildContext context, int index, Prediction prediction);
